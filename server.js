ec9640821fce7423f236b4287e18c0e209c1e31b
Yeahyun Kim <insidesy4@gmail.com>
2024-11-15 15:13:14 +0900

    [Add] 플업 프론트엔트 3000포트 서버 설정 파일

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const Bundler = require('parcel-bundler');
+
+const app = express();
+const port = 3000;
+
+// Parcel 번들러 설정
+const bundler = new Bundler(['public/home.html'], {
+outDir: './dist', // 번들된 파일들이 저장될 디렉토리
+    watch: true, // 파일 변경 감지
+});
+
+app.use(bundler.middleware());
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'home.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
