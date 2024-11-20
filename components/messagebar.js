b805878a0119906c5cb26ae2318d983f2b0784cc
Yeahyun Kim <insidesy4@gmail.com>
2024-11-20 16:17:17 +0900

    [Feat] messagebar 영역 드래그로 width값 조정 및 local storage에 width값을 저장하여 새로고침 시, 앞전에 설정한 width값으로 노출 #2

--- components/messagebar.js
+++ components/messagebar.js
@@ -1,3 +1,46 @@
 $(document).ready(function () {
-    
+    $('#messagebar-container').html(`
+        <div class="messagebar-nav" id="messagebar-nav">
+            <div class="project-title">2024 플업 프로젝트</div>
+
+            <div class="message-item channel"></div>
+
+            <div class="resizer" id="resizer"></div>
+        </div>
+    `);
+
+    const $messagebarNav = $('#messagebar-nav');
+    const $resizer = $('#resizer');
+
+    // 로컬 스토리지에서 너비 값 가져오기
+    const savedWidth = localStorage.getItem('messagebarNavWidth');
+    if (savedWidth) {
+        $messagebarNav.width(savedWidth);
+    }
+
+    let isResizing = false;
+
+    $(document).on('mousedown', '#resizer', function(event) {
+        isResizing = true;
+        const startX = event.clientX;
+        const startWidth = $messagebarNav.width();
+
+        $(document).on('mousemove', function(event) {
+            if (isResizing) {
+                const newWidth = startWidth - (startX - event.clientX);
+                $messagebarNav.width(newWidth);
+            }
+        });
+
+        $(document).on('mouseup', function() {
+            isResizing = false;
+            $(document).off('mousemove');
+            $(document).off('mouseup');
+
+            // 너비 값을 로컬 스토리지에 저장
+            localStorage.setItem('messagebarNavWidth', $messagebarNav.width());
+        });
+
+        event.preventDefault(); // 기본 동작 방지
+    });
 });
